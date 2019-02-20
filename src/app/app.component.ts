a87c3f792a95c022b65da145e585d80d774cb507
SmachuloTetiana <tanichk@ukr.net>
2019-02-20 11:03:04 +0200

    http first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  apiUrl = 'https://api.apixu.com/v1/current.json?key=fa8ff3731b154ea695d212733191902&q=Lviv';
+  weather: any = {};
+
+  constructor(private http: HttpClient) {
+    this.getWeather();
+  }
+
+  getWeather() {
+    this.http.get(this.apiUrl).subscribe(data => {
+      console.log(data);
+      this.weather = data
+    });
+  }
 }
